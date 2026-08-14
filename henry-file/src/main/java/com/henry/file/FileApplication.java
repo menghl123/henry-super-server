41209706ace429f3ad381aa02d763786f923c4f4
say-hai <mhlqian@qq.com>
2026-08-14 15:19:43 +0800

    chore:清空file server

--- henry-file/src/main/java/com/henry/file/FileApplication.java
+++ henry-file/src/main/java/com/henry/file/FileApplication.java
@@ -1,12 +1,10 @@
 package com.henry.file;
 
-import com.henry.file.infrastructure.config.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(FileProperties.class)
 public class FileApplication {
 
     public static void main(String[] args) {
